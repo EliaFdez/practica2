e2b1033b0dd70a6546ad5c70714f2fbb1a39cf01
EliaFdez <40912556+EliaFdez@users.noreply.github.com>
2022-03-17 11:43:43 +0100

    Add files via upload
    Primera versión: Que no tenga en cuenta los coches que hay esperando. Puede dar problemas de inanición (NO FUNCIONA)

---
+++ practica2_v1.py
@@ -0,0 +1,79 @@
+"""
+Solution to the one-way tunnel
+"""
+import time
+import random
+from multiprocessing import Lock, Condition, Process
+from multiprocessing import Value
+
+SOUTH = "south"
+NORTH = "north"
+
+NCARS = 10
+
+
+class Monitor():
+    def __init__(self):
+        self.mutex = Lock()
+        self.south = Value('i', 0)  # number of cars in south direction
+        self.north = Value('i', 0)  # number of cars in north direction
+        self.no_south = Condition(self.mutex)
+        self.no_north = Condition(self.mutex)
+
+    def wants_enter(self, direction):
+        self.mutex.acquire()
+
+        if direction == NORTH:
+            self.no_south.acquire(self.south.value == 0)
+            self.north.value = self.north.value + 1
+        else:
+            self.no_north.acquire(self.north.value == 0)
+            self.south.value = self.south.value + 1
+
+        self.mutex.release()
+
+    def leaves_tunnel(self, direction):
+        self.mutex.acquire()
+
+        if direction == NORTH:
+            self.north.value = self.north.value - 1
+            self.no_north.release()
+        else:
+            self.south.value = self.south.value - 1
+            self.no_south.release()
+
+        self.mutex.release()
+
+
+def delay(n=3):
+    time.sleep(random.random() * n)
+
+
+def car(cid, direction, monitor):
+    print(f"car {cid} direction {direction} created")
+    delay(6)
+    print(f"car {cid} heading {direction} wants to enter")
+    monitor.wants_enter(direction)
+    print(f"car {cid} heading {direction} enters the tunnel")
+    delay(3)
+
+    # numdir = monitor.north if direction == NORTH else monitor.south
+    # print('NUMEROS COCHES EN DIRECCION ' + direction + ' --> ' + numdir)
+
+    print(f"car {cid} heading {direction} leaving the tunnel")
+    monitor.leaves_tunnel(direction)
+    print(f"car {cid} heading {direction} out of the tunnel")
+
+
+def main():
+    monitor = Monitor()
+    cid = 0
+    for _ in range(NCARS):
+        direction = NORTH if random.randint(0, 1) == 1 else SOUTH
+        cid += 1
+        p = Process(target=car, args=(cid, direction, monitor))
+        p.start()
+        time.sleep(random.expovariate(1 / 0.5))  # a new car enters each 0.5s
+
+if __name__ == "__main__":
+    main()
